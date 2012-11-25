37412cb610ba693fb24b38151eb3ef1387347bec
Paul Graham <paul@bytefair.com>
2012-11-25 00:25:01 -0600

    engine established

---
+++ lib/engine.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This is the file that runs the site--the engine if you will. It holds the
+ * main function.
+ *
+ * This is a core file and should not be directly edited. Please use a child.
+ *
+ * @category    RAD
+ * @package     Core
+ * @subpackage  Engine
+ * @author      Paul Edmon Graham
+ * @license     http://www.mozilla.org/MPL/
+ * @link        http://github.com/bytefair/rad
+ */
+
+/**
+ * This is the main function of RAD. It loads the header footer, and basic
+ * hooks.
+ *
+ * @since 0.1
+ */
+function engine() {
+
+}
