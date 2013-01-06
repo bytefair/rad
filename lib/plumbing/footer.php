75e8890c4c2202fbdaa3f7a830a38fe9e4969b1f
Paul Graham <paul@bytefair.com>
2013-01-06 03:11:33 -0600

    starts the footer functions

---
+++ lib/plumbing/footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file contains functions called in footer.php in the theme root
+ *
+ * This is a core file and should not be directly edited. Please use a child.
+ *
+ * @category    RAD
+ * @package     Plumbing
+ * @subpackage  Footer
+ * @author      Paul Edmon Graham
+ * @license     http://www.mozilla.org/MPL/2.0/
+ * @link        http://github.com/bytefair/rad
+ */
