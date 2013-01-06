831fe053fd17530432cc65cb8554650041cf4b44
Paul Graham <paul@bytefair.com>
2013-01-06 02:21:21 -0600

    adds basic content hooks

--- lib/engine.php
+++ lib/engine.php
@@ -12,13 +12,22 @@
  * @link        http://github.com/bytefair/rad
  */
 
+
 /**
  * The main function of RAD. It loads the header, footer, and basic hooks.
  *
  * @since 0.1
  */
 function engine() {
-    get_header();
+    get_header(); ?>
 
+    <div class="site-content" role="main">
+        <?php
+        do_action( 'rad_before_content' );
+        do_action( 'rad_content' );
+        do_action( 'rad_after_content' ); ?>
+    </div><!-- end .site-content -->
+
+    <?php
     get_footer();
 }
