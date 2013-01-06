e1892c1c367f8c63588dfa2b0c39e78ab1969a5a
Paul Graham <paul@bytefair.com>
2013-01-06 03:21:49 -0600

    adds sidebar template

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Sidebar template
+ *
+ * This is a core file and should not be directly edited. Please use a child.
+ *
+ * @category    RAD
+ * @package     Templates
+ * @author      Paul Edmon Graham
+ * @license     http://www.mozilla.org/MPL/2.0/
+ * @link        http://github.com/bytefair/rad
+ * @since       0.1
+ */
