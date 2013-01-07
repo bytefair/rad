8d4890b1d31db1819daca1a9418e865b8adc8817
Paul Graham <paul@bytefair.com>
2013-01-06 23:35:51 -0600

    adds a function to display the generator credits and a filter for children
    Signed-off-by: Paul Graham <paul@bytefair.com>

--- lib/plumbing/footer.php
+++ lib/plumbing/footer.php
@@ -11,3 +11,19 @@
  * @license     http://www.mozilla.org/MPL/2.0/
  * @link        http://github.com/bytefair/rad
  */
+
+
+add_action( 'rad_footer', 'rad_generator_credits' );
+/**
+ * Displays the WordPress and RAD credits for the site
+ *
+ * @since 0.1
+ */
+function rad_generator_credits() {
+    $defaults  = __( 'This website forged using', 'rad' );
+    $defaults .= ' <a href="http://wordpress.org" title="';
+    $defaults .= esc_attr__( 'A Semantic Personal Publishing Platform', 'rad' );
+    $defaults .= '" rel="generator">WordPress</a> and ';
+    $defaults .= '<a href="http://bytefair.github.com/rad" title="RAD" rel="nofollow">RAD</a>';
+    echo apply_filters( 'generator_credits', $defaults );
+}
