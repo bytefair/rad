1f91fe7b2064c07c287fc5e7b078442c36799842
Paul Graham <paul@bytefair.com>
2013-01-14 08:19:15 -0600

    rethinking loops

--- lib/plumbing/loops.php
+++ lib/plumbing/loops.php
@@ -12,27 +12,13 @@
  * @link        http://github.com/bytefair/rad
  */
 
-
-add_action( 'rad_content', 'rad_logic_loop' );
+add_action( 'rad_content', 'rad_template_logic_loop' );
 /**
- * This loop handles the basic content logic and calls all necessary templates.
+ * This loop handles the basic parts of every loop and then grabs the logic.
  *
  * @since 0.1
  *
- * @uses rad_404() Default 404 for your site
- * @uses rad_page_loop()
- * @uses rad_single_loop()
- * @uses rad_archive_loop()
- * @uses rad_search()
- * @uses rad_image_loop()
- * @uses rad_default_loop()
  */
-function rad_logic_loop() {
-    if ( is_404() ) { rad_404();
-    } elseif ( is_page() ) { rad_page_loop();
-    } elseif ( is_single() ) { rad_single_loop();
-    } elseif ( is_archive() ) { rad_archive_loop();
-    } elseif ( is_search() ) { rad_search();
-    } elseif ( is_image() ) { rad_image_loop();
-    } else { rad_default_loop(); }
+function rad_template_logic_loop() {
+
 }
